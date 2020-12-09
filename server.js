0ef5b02a12c01a3f0a77f8c8963acf6095bb7830
Jake Mackinlay <jrmackinlay90@gmail.com>
2020-12-09 23:45:50 +1030

    corrected rendering issue and printing both devoured and non-devoured burgers. Need to finalize routes, buttons and style

--- server.js
+++ server.js
@@ -4,22 +4,22 @@ const path = require("path");
 
 const app = express();
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3005;
+
+const routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.use("/public",express.static(path.join(__dirname, "public")));
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json);
-
 
 app.set("view engine", "handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 
 // app.use(express.static("public"));
 
-app.use("/public",express.static(path.join(__dirname, "public")));
-
-const router = require("./controllers/burgers_controller.js");
-
-app.use(router);
 
 app.listen(PORT, function() {
     console.log(`App now listening on ${PORT}`);
