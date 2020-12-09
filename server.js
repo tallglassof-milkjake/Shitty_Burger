f028dcd5bf23be22b53981c889456099dcfc2a56
Jake Mackinlay <jrmackinlay90@gmail.com>
2020-12-09 22:32:16 +1030

    stashing changes to revert to an older version

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
+const path = require("path");
 
 const app = express();
 
-const PORT = process.env.PORT || 8080;
-
-const router = require("./controllers/burgers_controller.js");
-
-app.use(router);
+const PORT = process.env.PORT || 4000;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json);
 
+
 app.set("view engine", "handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
+
+app.use("/public",express.static(path.join(__dirname, "public")));
+
+const router = require("./controllers/burgers_controller.js");
+
+app.use(router);
 
 app.listen(PORT, function() {
     console.log(`App now listening on ${PORT}`);
