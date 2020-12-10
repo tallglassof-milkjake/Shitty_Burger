48c9c2a0211c1b36fc1bf14d931a3a65c3901394
Jake Mackinlay <jrmackinlay90@gmail.com>
2020-12-10 18:37:55 +1030

    submit burger to database function written. not perfected

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,3 +1,35 @@
 $(document).on("load", function() {
 
+    function submitBurger(event) {
+        event.preventDefault();
+        let inputField = $("#newBurger").val().trim();
+
+        if (inputField.length == 0) {
+            Swal("Sorry", "Try again friend", "warning");
+        } else {
+            try {
+                $.ajax("/", {
+                    type: 'POST',
+                    data: {
+                        inputField
+                    },
+                }).then(function(res) {
+                    console.log(res);
+                    Swal(`${inputField}`, "Thank You", "success");
+                });
+            } catch (error) {
+                throw (error);
+            }
+        }
+
+        let query = `INSERT INTO ?? (burger_name, devoured) VALUES (${inputField}, false)`;
+
+        connection.query(query, function(err, res) {
+        
+        })
+    }
+
+    $("#submitBTN").on("click", function() {
+        submitBurger();
+    })
 });
